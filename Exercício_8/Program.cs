8e96ad4b65dccf63ea915e2689b5bce2208f7fcb
ovitorguimaraes <ovitordeoliveiraguimaraes@gmail.com>
2025-08-24 22:13:08 -0300

    Subindo 8, 9

---
+++ Exercício_8/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número de quatro algarismos, vamos verificar se ele é palíndromo: ");
+        int num = int.Parse(Console.ReadLine());
+
+        if (palindromo(num))
+        {
+            Console.Write($"O número {num} é um palindromo.");
+        }
+
+        else
+        {
+            Console.Write($"O número {num} não é um palindromo.");
+        }
+    }
+
+    static bool palindromo(int num)
+    {
+        if (num < 1000 || num > 9999)
+        {
+            Console.Write("O numéro é invalido");
+            return false;
+        }
+
+        int n1 = num / 1000;
+        int n2 = (num / 100) % 10;
+        int n3 = (num / 10) % 10;
+        int n4 = num % 10;
+
+        return (n1 == n4 && n2 == n4);
+    }
+        
+}
---
+++ Exercício_9/Program.cs
@@ -0,0 +1,54 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[,] matriz = new int[3, 3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write($"Digite o valor do termo [{i}, {j}]: ");
+                matriz[i, j] = int.Parse(Console.ReadLine());
+            }
+        } // até aqui funciona, OK
+
+        // estudar
+        (int[] acima, int[] abaixo) = pegarelementos(matriz);
+
+        Console.Write("\nElementos acima da diagonal principal: ");
+        foreach (int valor in acima)
+            Console.Write($"\n{valor}");
+
+        Console.Write("\n\nElementos abaixo da diagonal principal:");
+        foreach (int valor in abaixo)
+            Console.Write($"\n{valor}");
+    }
+
+    static (int[], int[]) pegarelementos(int[,] matriz)
+    {
+        int tamanho = 3;
+        int qtd = (tamanho * (tamanho - 1)) / 2;
+        int[] acima = new int[qtd];
+        int[] abaixo = new int[qtd];
+        int k1 = 0, k2 = 0;
+
+        for (int i = 0; i < tamanho; i++)
+        {
+            for (int j = 0; j < tamanho; j++)
+            {
+                if (j > i)
+                {
+                    acima[k1++] = matriz[i, j];
+                }
+                else if (j < i)
+                {
+                    abaixo[k2++] = matriz[i, j];
+                }
+            }
+        }
+
+        return (acima, abaixo);
+    }
+}
+// estudar
