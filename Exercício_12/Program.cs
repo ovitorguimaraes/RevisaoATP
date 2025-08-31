1ebdff27da552dac4c3b4008f9c100932dfae328
ovitorguimaraes <ovitordeoliveiraguimaraes@gmail.com>
2025-08-31 19:24:27 -0300

    Subindo questão 12

---
+++ Exercício_12/Program.cs
@@ -0,0 +1,50 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[,] matriz = new int[2, 5];
+        int[,] matrizfinal = new int[2, 5];
+
+        int k;
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Console.Write($"Digite o valor da posição [{i}, {j}]: ");
+                matriz[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        Console.Write("Digite o valor da escalar: ");
+        k = int.Parse(Console.ReadLine());
+
+        matrizfinal = OperacaoMatriz(matriz, k);
+
+        Console.Write("O valor dos termos da matriz somados ao número escalar é: \n");
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Console.Write($"{matrizfinal[i, j]}\n");
+            }
+        }
+    }
+
+
+    static int[,] OperacaoMatriz(int[,] matriz, int k)
+    {
+        int[,] calculo = new int[2, 5];
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                calculo[i, j] = matriz[i,j] + k;
+            }
+        }
+
+        return calculo;
+    }
+}
